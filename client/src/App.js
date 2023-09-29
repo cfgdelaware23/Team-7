f466423dd4e7a945eb230ee962a81ebfce1b428b
caramurphyy <murphylcara@gmail.com>
2023-09-29 19:16:28 -0400

    bg image and basic text

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button, Typography} from '@mui/material';
+
 import Form from'./form.js';
 
 function App() {
   return (
-    <><Form /><div className="App">
-    
-    </div></>
+    //<Navbar/>
+    <div className="home">
+      <Typography variant="h1">
+      Welcome!
+      </Typography>
+
+
+      <Typography variant="body1" type="left">
+      Creating a seamless grocery shopping experience, with<br></br>
+      <i> personalized pricing.</i>
+      </Typography>
+
+      <Typography variant="body1">
+      Do you have an account with us?
+      </Typography>
+
+      <Button variant="contained" class="homeButton">Yes</Button>
+      <Button variant="contained" class="homeButton">No</Button>
+    </div>
   );
 }
 

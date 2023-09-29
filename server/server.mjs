dacd64d7b2f0b5d6aa9586482da90c74b57fc51c
Elizabeth Moon <69644849+elizabethmoonw@users.noreply.github.com>
2023-09-29 19:39:43 -0400

    add server code to connect mongodb

--- server/server.mjs
+++ server/server.mjs
@@ -1,17 +1,35 @@
-import express from "express";
-import cors from "cors";
-import "./loadEnvironment.mjs";
-import records from "./routes/record.mjs";
-
-const PORT = process.env.PORT || 5050;
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
 const app = express();
+const PORT = 5000;
+const url = 'mongodb://localhost:27017';
+const dbName = 'myFormDB';
+let db;
 
+// Connect to MongoDB
+MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+    if (err) {
+        console.error('Failed to connect to the database', err);
+        process.exit(1);
+    }
+    console.log('Connected to MongoDB');
+    db = client.db(dbName);
+});
 app.use(cors());
-app.use(express.json());
-
-app.use("/record", records);
-
-// start the Express server
+app.use(bodyParser.json());
+app.post('/submit-form', (req, res) => {
+    const formData = req.body;
+    db.collection('formResponses').insertOne(formData, (err, result) => {
+        if (err) {
+            console.error('Failed to insert form data', err);
+            res.status(500).send({ message: 'An error occurred' });
+            return;
+        }
+        res.send({ message: 'Form data submitted successfully' });
+    });
+});
 app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });

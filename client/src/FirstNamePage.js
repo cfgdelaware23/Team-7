b17c2213c699a3ea04349460ec7d4232c6a49c40
caramurphyy <murphylcara@gmail.com>
2023-09-29 22:33:22 -0400

    move form styling to formpages.css for reusability

--- client/src/FirstNamePage.js
+++ client/src/FirstNamePage.js
@@ -1,35 +1,28 @@
 import './FormPages.css';
 import { useState } from 'react';
-import { TextField, Box , Button} from "@mui/material";
+import { TextField, Box , Button, Typography} from "@mui/material";
 
-export default function FirstNameComponent() {
+export default function FirstPage() {
   const [firstName, setFirstName] = useState('');
 
-  // props: current user schema, question, setQuestion 
-
-
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '100vh',
-    }}>
-      <label style={{ fontSize: '50px', marginBottom: '16px' }}>
+    <div class="formContainer">
+      <Typography variant="h1">
         First Name:
-      </label>
-      <Box>
+      </Typography>
+      <div class="inputContainer">
         <TextField
           value={firstName}
           onChange={e => setFirstName(e.target.value)}
-          style={{ width: '600px', fontSize: '40px'}} 
+          sx={{ width: '40rem', backgroundColor:'white' }} 
+          inputProps={{style: {fontSize: '2rem'}}} // font size of input text
+          InputLabelProps={{style: {fontSize: '2rem'}}}
         />
-        <Box>
-          <Button>Back</Button>
-          <Button style={{ marginLeft: '475px' }}>Next</Button>
-      </Box>
-      </Box>
+         <div class="buttonContainer">
+          <Button variant="contained" class="formButton" disableRipple>Back</Button>
+          <Button variant="contained" class="formButton" disableRipple>Next</Button>
+        </div>
+      </div>
     </div>
   );
 }

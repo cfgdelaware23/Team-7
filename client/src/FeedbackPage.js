367a0730f4efb2cf98576f407c5927e27452bf23
Psedghi <76452130+Psedghi@users.noreply.github.com>
2023-09-30 08:11:59 -0400

    updates on feedback

--- client/src/FeedbackPage.js
+++ client/src/FeedbackPage.js
@@ -1,13 +1,8 @@
 import './App.css';
-import {Button, Typography} from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import "./FeedbackPage.css"
 import React, { useState, useEffect } from 'react';
-// import fs from 'fs/promises'; // Node.js file system module
-
-// function RandomFood(props) {
-//   return <li>I am a {props.items}</li>;
-// }
 
 export default function BoughtItems() {
   const items = ['strawberries', 'milk'];
@@ -17,12 +12,12 @@ export default function BoughtItems() {
   const [responses, setResponses] = useState([]);
 
   useEffect(() => {
-    // Load responses from the JSON file when the component mounts
-    async function loadResponses() { 
+    async function loadResponses() {
       try {
-      //  const responseFileContents = await fs.readFile('responses/response.json', 'utf8');
-      //  const parsedResponses = JSON.parse(responseFileContents);
-      //  setResponses(parsedResponses);
+        // Load responses from the JSON file when the component mounts
+        // const responseFileContents = await fs.readFile('responses/response.json', 'utf8');
+        // const parsedResponses = JSON.parse(responseFileContents);
+        // setResponses(parsedResponses);
       } catch (error) {
         console.error('Error loading responses:', error);
       }
@@ -41,7 +36,7 @@ export default function BoughtItems() {
     // Update the responses in the JSON file
     async function updateResponsesFile() {
       try {
-       // await fs.writeFile('responses/response.json', JSON.stringify(newResponses), 'utf8');
+        // await fs.writeFile('responses/response.json', JSON.stringify(newResponses), 'utf8');
         setResponses(newResponses);
       } catch (error) {
         console.error('Error updating responses:', error);
@@ -51,33 +46,29 @@ export default function BoughtItems() {
     updateResponsesFile();
   };
 
-  let imageUrl= require('./assets/strawberries.png');
+  let imageUrl = require('./assets/strawberries.png');
   if (items[randomIndex] === 'strawberries') {
-    imageUrl = require('./assets/strawberries.png')
+    imageUrl = require('./assets/strawberries.png');
   } else {
-    imageUrl = require('./assets/milk.png')
+    imageUrl = require('./assets/milk.png');
   }
 
   return (
-    <>
     <div className="home">
-    <div className="homeInfoContainer">
-    <Typography variant="body1" type="left" className="body">
-      <h1>How did you feel about the {items[randomIndex]} you bought?</h1>
-    </Typography>
-    </div>
-    <div className="homeButtonContainer">
-      <div class="centered-div">
-      <Button variant="contained" className="buttonText" disableRipple onClick={() => handleResponse(1)}>
-          Good </Button>
+      <div className="homeInfoContainer">
+        <Typography variant="body1" type="left" className="body">
+          <h1>How did you feel about the {items[randomIndex]} you bought?</h1>
+        </Typography>
+      </div>
+      <div className="centered-div">
+        <Button variant="contained" className="buttonText" disableRipple onClick={() => handleResponse(1)}>
+          Good
+        </Button>
         <img src={imageUrl} alt="Product" />
         <Button variant="contained" className="buttonText" disableRipple onClick={() => handleResponse(-1)}>
           Bad
         </Button>
-        </div>
       </div>
     </div>
-    </>
   );
 }
-

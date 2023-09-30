73dedc98fd2620305b4805f8b7fe5e1f0d1d23f8
Psedghi <76452130+Psedghi@users.noreply.github.com>
2023-09-30 01:07:35 -0400

    added a timer to the congrats page

--- client/src/Componets/congrats.js
+++ client/src/Componets/congrats.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate from React Router
 
 function CongratsPage({ onSignUp }) {
+  const navigate = useNavigate(); // Create a navigate function
+
+  useEffect(() => {
+    // Use setTimeout to delay the redirection for 3 seconds (adjust the time as needed)
+    const redirectTimeout = setTimeout(() => {
+      navigate('/'); // Use navigate('/') to go back to the home page
+    }, 3000); // 3000 milliseconds (3 seconds)
+
+    // Clear the timeout when the component unmounts to prevent memory leaks
+    return () => clearTimeout(redirectTimeout);
+  }, [navigate]);
+
   const congratsPageStyle = {
     fontFamily: 'Roboto, sans-serif', // Apply the Roboto font
     textAlign: 'center',
@@ -16,7 +29,6 @@ function CongratsPage({ onSignUp }) {
     <div style={congratsPageStyle} className="congrats-page">
       <h3>Congratulations! </h3>
       <p>You have successfully signed up.</p>
-      <button onClick={handleSignUp}>Back to Home</button>
     </div>
   );
 }

b3c6cc1a1a19f05ebda9ade2206f53d330cef574
Psedghi <76452130+Psedghi@users.noreply.github.com>
2023-09-30 01:55:20 -0400

    outline comments

---
+++ client/src/SpotifyWrap.js
@@ -0,0 +1,9 @@
+//I just want to make an outline of what I want to make for this file.
+//Some of the features include: 
+//Displaying 
+//-customer purchasing data,
+//-Nutrition of products (compared to typical foods at Dollar General or 7/11),
+//-Total Saved YTD (compared to competitors retail)
+//-Favorite Purchases
+
+

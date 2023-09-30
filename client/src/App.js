9f4e096123bb635ffb94f1a988c0ddd81876f254
caramurphyy <murphylcara@gmail.com>
2023-09-29 20:49:30 -0400

    move images fix bg

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,33 @@
 import './App.css';
 import {Button, Typography} from '@mui/material';
+import '@fontsource/roboto/300.css';
 
 import Form from'./form.js';
 
 function App() {
   return (
     //<Navbar/>
-    <div className="home">
-      <Typography variant="h1">
+    <div class="home">
+      <Typography variant="h1" class="welcomeText">
       Welcome!
       </Typography>
 
+      <div class = "homeInfoContainer">
+        <Typography variant="body1" type="left" class="infoText">
+        Creating a seamless grocery shopping experience, with<br></br>
+        <i> personalized pricing.</i>
+        </Typography>
+        <img src={require("./images/homeImage.png")} class ="homeInfoImage" alt="A World Well Nourished"></img>
+      </div>
 
-      <Typography variant="body1" type="left">
-      Creating a seamless grocery shopping experience, with<br></br>
-      <i> personalized pricing.</i>
-      </Typography>
-
-      <Typography variant="body1">
+      <Typography variant="body1" class="promptText">
       Do you have an account with us?
       </Typography>
-
-      <Button variant="contained" class="homeButton">Yes</Button>
-      <Button variant="contained" class="homeButton">No</Button>
+     
+      <div class = "homeButtonContainer">
+        <Button variant="contained" class="homeButton" disableRipple>Yes</Button>
+        <Button variant="contained" class="homeButton" disableRipple>No</Button>
+      </div>
     </div>
   );
 }

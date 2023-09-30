ce6f949c8e0307ec25edc267f694407528806a0f
m <94079601+ky322@users.noreply.github.com>
2023-09-29 21:30:15 -0400

    feedback

--- feedback.py
+++ feedback.py
@@ -1,28 +1,21 @@
 import random
 
-class GroceryStore:
-    def __init__(self):
-        self.visits = []
+items = ["carrots", "bread", "milk", "eggs", "cheese", "apples", "oranges", "chocolate", "coffee", "tea"]
+visits = []
 
-    def visit_store(self, items_bought):
-        if self.visits:
-            past_visit = random.choice(self.visits)
-            item_for_feedback = random.choice(past_visit)
-            feedback = input(f"How was the {item_for_feedback} you bought last time? (good/bad) ")
-            print(f"Thank you for your feedback on the {item_for_feedback}. You mentioned it was {feedback}.")
+for _ in range(10):
+    random_items = random.sample(items, random.randint(1, len(items)))
+    visits.append(random_items)
 
-        self.visits.append(items_bought)
-        print("Items added to your shopping list:", ', '.join(items_bought))
+def get_feedback_from_visit():
+    random_visit = random.choice(visits)
+    random_item = random.choice(random_visit)
 
-    def get_past_visits(self):
-        return self.visits
+    feedback = input(f"How was the {random_item} you bought last time? (good/bad) ")
+    print(f"Thank you for your feedback on the {random_item}.\n")
 
-store = GroceryStore()
-while True:
-    items_bought = input("Enter items bought today separated by commas: ").split(',')
-    items_bought = [item.strip() for item in items_bought]
-    store.visit_store(items_bought)
-    
-    cont = input("Do you wish to simulate another visit? (yes/no) ").lower()
-    if cont == 'no':
-        break
+    print("Your receipt from the selected visit:")
+    for item in random_visit: 
+        print(f"- {item}")
+
+get_feedback_from_visit()
